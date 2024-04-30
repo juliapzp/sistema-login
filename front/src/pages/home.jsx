7c0afa770bf36429da64c6d9d260885ef59d18ec
JÚLIA PICKCIUS ZANIN PEREIRA <julia_p_pereira@estudante.sesisenai.org.br>
2024-04-30 09:07:29 -0300

    Versão 1.0

---
+++ front/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../App.css'
+
+function Home() {
+    return(
+    <div>
+        <h1>Página Inicial</h1>
+        <h2>Júlia Pereira</h2>
+    </div>
+    )
+}
+
+export default Home;
